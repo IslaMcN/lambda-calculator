6d3c17005256bca8a2423a768d70ca2f7c1aee32
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-08-14 08:26:58 -0700

    Added some exports

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export const NumberButton = (props) => {
   return (
-    <button className="number-button" onClick={() => props.addNumber(props.text)}>
+    <button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.text}
     </button>
   );
 };
+export default NumberButton;
